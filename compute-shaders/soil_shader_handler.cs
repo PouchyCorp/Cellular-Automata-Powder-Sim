cb55a4566d86f5491a62082aad684e9a799463a6
Paul Baumard <92679451+PouchyCorp@users.noreply.github.com>
2026-09-24 10:40:51 +0200

    last compute shader effort, will try to optimise the CPU side to see if it is even worth it to pursue

--- compute-shaders/soil_shader_handler.cs
+++ compute-shaders/soil_shader_handler.cs
@@ -1,31 +1,38 @@
 using Godot;
+using Godot.NativeInterop;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlTypes;
 using System.IO;
+using System.Runtime.InteropServices;
 
+[StructLayout(LayoutKind.Sequential)]
+public struct Cell
+{
+    public float Nutrient;
+    public uint Present;
+}
 
 public class SoilShaderHandler
 {
     private RenderingDevice rd;
-    private RDShaderFile shaderFile;
-    private RDShaderSpirV shaderBytecode;
     public Rid shader;
 
-    private Image inputImage;
+    public List<Cell> inputA;
+    public List<Cell> inputB;
 
-    public void Initialize()
-    {
-        rd = RenderingServer.CreateLocalRenderingDevice();
-        shaderFile = GD.Load<RDShaderFile>("res://compute_example.glsl");
-        shaderBytecode = shaderFile.GetSpirV();
-        shader = rd.ShaderCreateFromSpirV(shaderBytecode);
-    }
+    public Rid RidInputA;
+    public Rid RidInputB;
+
+    public Rid pipeline;
+
 
 
     public void SetShaderData(Element[,] elementArray, int width, int height){
         // Prepare the data for the shader. red channel is nutrients, green channel is at 1.0 for soil
         // Ideally we could update the image when changes are made to the soil to avoid having to iterate through the entire array every frame, but for now this is fine.
-        inputImage = new Image();
+        inputA = [];
         // Set the image data here
         for (int y = 0; y < height; y++)
         {
@@ -43,24 +50,20 @@ public class SoilShaderHandler
 
                 // Set the pixel color based on the element properties
                 Color pixelColor = new Color(nutrients, soilPresence, 0.0f, 1.0f);
-                inputImage.SetPixel(x, y, pixelColor);
+                inputA.Add(new Cell { Nutrient = nutrients, Present = (uint)soilPresence });
             }
         }
+
+        inputB = inputA;
     }
 
-    public Image getResult(){
+    public void getResult(){
         // To be called a few frames after RunCompute() to get the result of the compute shader.
 
     }
 
     public void RunCompute(){
-        // Prepare our data. We use floats in the shader, so we need 32 bit.
 
-        Image.BlockCopy(input, 0, inputBytes, 0, inputBytes.Length);
-
-        // Create a storage buffer that can hold our float values.
-        // Each float has 4 bytes (32 bit) so 10 x 4 = 40 bytes
-        var buffer = rd.StorageBufferCreate((uint)inputBytes.Length, inputBytes);
     }
 
     public void InitGPU(){
@@ -74,57 +77,70 @@ public class SoilShaderHandler
         }
 
         //# Prepare the shader.
-        shaderFile = GD.Load<RDShaderFile>("res://compute_example.glsl");
+        RDShaderFile shaderFile = GD.Load<RDShaderFile>("res://soil.glsl");
+        RDShaderSpirV shaderBytecode = shaderFile.GetSpirV();
+        shader = rd.ShaderCreateFromSpirV(shaderBytecode);
 
-        //# Create format for heightmap.
-        RDTextureFormat input_format = new RDTextureFormat();
-        //# There are a lot of different formats. It might take some studying to be able to be able to
-        //# choose the right ones. In this case, we tell it to interpret the data as a single byte for red.
-        //# Even though the noise image only has a luminance channel, we can just interpret this as if it
-        //# was the red channel. The byte layout is the same!
-        heightmap_format.format = RenderingDevice.DataFormat.rgb
-        heightmap_format.width = po2_dimensions
-        heightmap_format.height = po2_dimensions
-        //# The TextureUsageBits are stored as 'bit fields', denoting what can be done with the data.
-        //# Because of how bit fields work, we can just sum the required ones: 8 + 64 + 128
-        heightmap_format.usage_bits = \
-                RenderingDevice.TEXTURE_USAGE_STORAGE_BIT + \
-                RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT + \
-                RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
 
-        //# Prepare heightmap texture. We will set the data later.
-        heightmap_rid = rd.texture_create(heightmap_format, RDTextureView.new())
 
-        //# Create uniform for heightmap.
-        var heightmap_uniform := RDUniform.new()
-        heightmap_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-        heightmap_uniform.binding = 0  //# This matches the binding in the shader.
-        heightmap_uniform.add_id(heightmap_rid)
+        uint cellSize = (uint)Marshal.SizeOf<Cell>();
+        
+        if (cellSize == 8) {
+            GD.PushError("""Cell struct is not 8 bytes in size, the shader will not work""");
+        }
 
-        //# Create format for the gradient.
-        var gradient_format := RDTextureFormat.new()
-        //# The gradient could have been converted to a single channel like we did with the heightmap,
-        //# but for illustrative purposes, we use four channels (RGBA).
-        gradient_format.format = RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM
-        gradient_format.width = gradient_tex.width  //# Default: 256
-        //# GradientTexture1D always has a height of 1.
-        gradient_format.height = 1
-        gradient_format.usage_bits = \
-            RenderingDevice.TEXTURE_USAGE_STORAGE_BIT + \
-            RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT
 
-        //# Storage gradient as texture.
-        gradient_rid = rd.texture_create(gradient_format, RDTextureView.new(), [gradient_tex.get_image().get_data()])
+        // Initialize the input and output buffers with the same size and data 
+        // We try to map the data to the shader in a way that is compatible with the shader's expectations.
+        long bufferSize = inputA.Count * cellSize;
+        byte[] initialData = new byte[inputA.Count * cellSize];
 
-        //# Create uniform for gradient.
-        var gradient_uniform := RDUniform.new()
-        gradient_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-        gradient_uniform.binding = 1  # This matches the binding in the shader.
-        gradient_uniform.add_id(gradient_rid)
+        for (int i = 0; i < inputA.Count; i++)
+        {
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(inputA[i].Nutrient),
+                0,
+                initialData,
+                (int)(i * cellSize),
+                4
+            );
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(inputA[i].Present),
+                0,
+                initialData,
+                (int)(i * cellSize + 4),
+                4
+            );
+        }
+        RidInputA = rd.StorageBufferCreate((uint)bufferSize, initialData);
+        RidInputB = rd.StorageBufferCreate((uint)bufferSize, initialData);
 
-        uniform_set = rd.uniform_set_create([heightmap_uniform, gradient_uniform], shader_rid, 0)
 
-        pipeline = rd.compute_pipeline_create(shader_rid)
+        RDUniform input_uniform = new RDUniform();
+        input_uniform.UniformType = RenderingDevice.UniformType.StorageBuffer;
+        input_uniform.Binding = 0;
+        input_uniform.AddId(RidInputA);
+
+        RDUniform output_uniform = new RDUniform();
+        output_uniform.UniformType = RenderingDevice.UniformType.StorageBuffer;
+        output_uniform.Binding = 1;
+        output_uniform.AddId(RidInputB);
+
+
+        // these will roll
+        Rid uniform_set_AB = rd.UniformSetCreate([input_uniform, output_uniform], shader, 0);
+        Rid uniform_set_BA = rd.UniformSetCreate([output_uniform, input_uniform], shader, 0);
+
+        pipeline = rd.ComputePipelineCreate(shader);
+    }
+
+    public void InitChangeBuffer(){
+        // Enough room for however many changes you expect in one frame.
+        const int MaxChanges = 4096;
+
+        _changeBuffer = _rd.StorageBufferCreate(
+            MaxChanges * ChangeSize
+        );
     }
 
     public void CleanupGPU(){
@@ -150,5 +166,6 @@ public class SoilShaderHandler
         rd.free()
         rd = null
 
+    }
 }
 
